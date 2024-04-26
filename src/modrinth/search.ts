bd604d552bf82d6d82ec54cf289005ba7cc25755
encode42 <me@encode42.dev>
2024-04-25 21:54:55 -0400

    Ignore Modrinth server-side errors

--- src/modrinth/search.ts
+++ src/modrinth/search.ts
@@ -1,4 +1,4 @@
-import type { SearchResultHit } from "@xmcl/modrinth";
+import { ModerinthApiError, type SearchResult, type SearchResultHit } from "@xmcl/modrinth";
 import type { SearchIndex } from "../types/SearchIndex";
 import { client } from "./client";
 import { log } from "../log";
@@ -11,10 +11,22 @@ export async function search(facets: string, index: SearchIndex = "newest") {
 	log.info("Searching Modrinth for new projects...");
 	log.debug(facets);
 
-	const response = await client.searchProjects({
-		"index": "newest",
-		"facets": facets
-	});
+	let response: SearchResult;
+	try {
+		response = await client.searchProjects({
+			"index": "newest",
+			"facets": facets
+		});
+	} catch (error) {
+		if (error instanceof ModerinthApiError && error.status >= 500 && error.status < 600) {
+			log.error("Failed to search! Will try again during next interval...");
+			log.debug(error);
+
+			return [];
+		}
+
+		throw error;
+	}
 
 	log.info("Finished searching!");
 	return response.hits as CorrectHit[];
