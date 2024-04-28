d0e28410a20a15c5899b689a326ae619a01e0b4c
encode42 <me@encode42.dev>
2024-04-28 05:21:52 -0400

    Create build script for compiling binaries

---
+++ build.ts
@@ -0,0 +1,56 @@
+import { $ } from "bun";
+import { log } from "./src/log";
+
+interface BuildOptions {
+	"fileName": string;
+	"target": string;
+	"arch"?: string;
+}
+
+const targets = {
+	"linux": {
+		"label": "Linux",
+		"arm": true
+	},
+	"darwin": {
+		"label": "macOS",
+		"arm": true
+	},
+	"windows": {
+		"label": "Windows",
+		"arm": false
+	}
+};
+
+async function build({ fileName, target, arch = "x64-modern" }: BuildOptions) {
+	if (arch === "arm64") {
+		fileName += "-arm";
+	}
+
+	const result = await $`bun build --compile --minify --sourcemap src/index.ts --target=bun-${target}-${arch} --outfile build/${fileName}`.text();
+	log.debug(result);
+}
+
+for (const [target, value] of Object.entries(targets)) {
+	log.info(`Building for ${value.label}...`);
+
+	const options: BuildOptions = {
+		"fileName": `chiron-${value.label.toLowerCase()}`,
+		"target": target
+	};
+
+	await build(options);
+
+	if (value.arm) {
+		log.info("Also compiling for ARM...");
+
+		await build({
+			...options,
+			"arch": "arm64"
+		});
+
+		log.info("ARM binary built!");
+	}
+
+	log.info(`Finished building for ${value.label}!`);
+}
